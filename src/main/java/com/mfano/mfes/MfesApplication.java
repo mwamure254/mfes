ac8a60640a6d4ef21d8d3d5021ecb41ac7819308
mwamure254 <mwamuredkwicha@gmail.com>
2026-08-16 16:59:20 +0300

    mfes

---
+++ src/main/java/com/mfano/mfes/MfesApplication.java
@@ -0,0 +1,13 @@
+package com.mfano.mfes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mfano/mfes/MfesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mfano.mfes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
